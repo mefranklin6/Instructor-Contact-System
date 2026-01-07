e0276df1c188304835d7939b923fea62a899610f
mefranklin6 <mefranklin@csuchico.edu>
2026-01-07 11:36:45 -0800

    create hello world

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+"""
+This is the main entry file
 
+This file should:
+- Import the individual modules
+- Run Flet
+"""
+
+import flet as ft
+
+
+class InstructorContactSystem:
+    def __init__(self):
+        pass
+
+    def main(self, page: ft.Page):
+        page.title = "Instructor Contact System"
+        page.add(ft.Text("Hello World!"))
+
+
+if __name__ == "__main__":
+    app = InstructorContactSystem()
+    ft.app(target=app.main, port=8080, view=ft.AppView.WEB_BROWSER)
