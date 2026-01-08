0f38c05edfe99fb4aec02d14c2522cbac7050d77
mefranklin6 <mefranklin@csuchico.edu>
2026-01-08 08:22:21 -0800

    log errors

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+import logging as log
+
+
 def raise_error_window(message: str, title: str = "Error") -> None:
     print(f"{title}: {message}")
+    log.error(f"{title}: {message}")
     # TODO: Raise pop-up window in Flet
