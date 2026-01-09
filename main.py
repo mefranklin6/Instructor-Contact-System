c0bf81437a85f93716aeea876838cb34c928bbd0
mefranklin6 <mefranklin@csuchico.edu>
2026-01-08 16:59:33 -0800

    setup for testing, POC

--- main.py
+++ main.py
@@ -8,21 +8,27 @@ This file should:
 
 import logging as log
 import os
+from datetime import datetime
 
 import flet as ft
 
 from src import utils
+from src.data_loader import data_loader
 
 LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "DEBUG").upper()
 log.basicConfig(
     level=getattr(log, LOGGING_LEVEL, log.DEBUG),
     format="%(asctime)s %(levelname)s %(name)s %(message)s",
 )
+print(f"Logging level set to {LOGGING_LEVEL}")
 
 
 class InstructorContactSystem:
     def __init__(self):
-        pass
+        self.supported_locations_filter = False
+        if os.getenv("SUPPORTED_LOCATIONS_FILE_PATH"):
+            self.supported_locations_filter = True
+            import src.chico_supported_locations.parser as slp
 
     def main(self, page: ft.Page):
         page.title = "Instructor Contact System"
@@ -30,13 +36,43 @@ class InstructorContactSystem:
 
 
 if __name__ == "__main__":
-    import os
+    pass
 
-    import src.data_loader.data_loader as data_loader
+    # ---- Test for SupportedLocationsParser module ----
+    log.warning("Testing SupportedLocationsParser module...")
+    import src.chico_supported_locations.parser as slp
 
-    log.warning("Testing!!!")
-    loader = data_loader.DataLoader(file_path="FacilitiesLinkClassScheduleDaily.csv")
-    df = loader.load_data()
+    parser = slp.SupportedLocationsParser("Supported Locations.csv")
+    locations = parser.run()
+    # print(locations)
 
-    app = InstructorContactSystem()
-    ft.run(target=app.main, port=8080, view=ft.AppView.WEB_BROWSER)
+    # ---- Test for DataLoader module ----
+    log.warning("Testing DataLoader module...")
+    loader = data_loader.DataLoader(
+        file_path="FacilitiesLinkClassScheduleDaily.csv", supported_locations=locations
+    )
+    date = datetime(2025, 10, 1)
+    df = loader.semester_data(date)
+    df.to_csv("test_output.csv", index=False)
+
+    # # ---- Test for Aggregator module ----
+    log.warning("Testing Aggregator module...")
+    import src.aggregation.aggregator as agg
+    import json
+
+    aggregator = agg.Aggregator(df=df)
+    contact_dict = aggregator.by_instructor()
+
+    # Save as JSON
+    with open("aggregated_output.json", "w") as f:
+        json.dump(contact_dict, f, indent=2)
+
+    print(f"\nFound {len(contact_dict)} instructors")
+
+    # b = aggregator.by_location()
+    # print(f"Found {len(b)} locations")
+    # for location, instructors in list(b.items()):
+    #     print(f"{location}: {instructors}")
+
+    # app = InstructorContactSystem()
+    # ft.run(target=app.main, port=8080, view=ft.AppView.WEB_BROWSER)
