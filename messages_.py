127f89419aa1c966a825550cb07eaf47f550a23f
mefranklin6 <mefranklin@csuchico.edu>
2026-02-09 14:00:46 -0800

    created

---
+++ messages_.py
@@ -0,0 +1,10 @@
+# '{locations}' will be replaced with a list of every supported classroom that an instructor teaches in
+default_semester_start_message = """Hello,
+We are staff of Classroom Technology Services, and we are reaching out regarding the technology in : {locations}
+
+If any of the technology isn't working as expected, or if there's anything we can do to better support your teaching experience, please don't hesitate to reply to this email and let us know.
+
+Best regards,
+Classroom Technology Services
+classroom@csuchico.edu
+530-898-5475"""
