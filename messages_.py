ae4b35f33dd3af1e04a621bef610176bd5df7b39
mefranklin6 <mefranklin@csuchico.edu>
2026-02-11 11:57:34 -0800

    specify supported locations

--- messages_.py
+++ messages_.py
@@ -2,7 +2,7 @@ default_semester_start_subject = "Classroom Technology Check-In"
 
 # '{locations}' will be replaced with a list of every supported classroom that an instructor teaches in
 default_semester_start_message = """Hello,
-We are staff of Classroom Technology Services, and we are reaching out regarding the technology in : {locations}
+We are staff of Classroom Technology Services, and we are reaching out regarding the technology in our supported location(s): {locations}
 
 If any of the technology isn't working as expected, or if there's anything we can do to better support your teaching experience, please don't hesitate to reply to this email and let us know.
 
