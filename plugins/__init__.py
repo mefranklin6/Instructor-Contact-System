c625d16726c1ec88d9b8516dae429dff6f8c69c6
mefranklin6 <mefranklin@csuchico.edu>
2026-02-20 11:54:53 -0800

    created. (just a dosctring)

---
+++ plugins/__init__.py
@@ -0,0 +1,7 @@
+"""Plugins for the Instructor Contact System.
+
+This system supports a variety of data sources and external systems.
+
+If your campus uses a different system than what is included,
+consider writing a plugin and sending in a pull request so everyone benefits.
+"""
