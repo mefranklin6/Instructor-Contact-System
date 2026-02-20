cb64895134083d5eebdd188255cc454ca5abc3e3
mefranklin6 <mefranklin@csuchico.edu>
2026-02-20 14:58:01 -0800

    del

--- src/plugins/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Plugin layer.
-
-This package provides a small registry + loader for swapping data source modules
-(ID->email, schedule loader, supported locations) without changing core code.
-"""
--- src/plugins/loader.py
+++
@@ -1,6 +0,0 @@
-"""Plugin loader package.
-
-External plugin support has been removed.
-To add a new plugin, submit a PR with an implementation under plugins/
-and a new key branch in src/plugins/system_plugins.py.
-"""
